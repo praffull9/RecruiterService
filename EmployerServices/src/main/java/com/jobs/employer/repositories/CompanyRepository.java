7037dbbc4cf53fbda3e3f3b86e8834cc66ee421c
praffull9 <123364874+praffull9@users.noreply.github.com>
2023-01-23 15:56:43 +0530

    Delete EmployerServices directory

--- EmployerServices/src/main/java/com/jobs/employer/repositories/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobs.employer.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jobs.employer.entities.Company;
-
-public interface CompanyRepository extends JpaRepository<Company, Integer> {
-
-}
--- EmployerServices/src/main/java/com/jobs/employer/repositories/RecruiterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jobs.employer.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.jobs.employer.entities.Recruiter;
-
-public interface RecruiterRepository extends JpaRepository<Recruiter, Integer> {
-
-}
